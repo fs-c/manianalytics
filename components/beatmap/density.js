db2865404f734b66308ce3250a9fe31c1fdb7771
lw <root@fsoc.space>
2022-11-12 16:46:52 +0100

    Clean up density visualization

--- components/beatmap/density.js
+++ components/beatmap/density.js
@@ -1,6 +1,4 @@
-import * as colors from 'tailwindcss/colors'
 import * as Plot from "@observablehq/plot";
-import * as d3 from "d3";
 import { useEffect, useRef } from 'react';
 
 const eventsToActions = (events) => {
@@ -14,27 +12,16 @@ const eventsToActions = (events) => {
     return actions;
 };
 
-const getActionDensity = (actions) => {
-    const densityPrecision = 1000;
-
-    const density = [];
-
-    for (const action of actions) {
-        const i = Math.floor(action / densityPrecision);
-
-        density[i] = density[i] ? density[i] + 1 : 1;
-    }
-
-    return density;
-};
-
 export const BeatmapDensityVisualisation = ({ beatmap }) => {
     const actions = eventsToActions(beatmap.events);
 
     const containerRef = useRef();
 
     useEffect(() => {
+        const { width } = containerRef.current.getBoundingClientRect();
+
         const chart = Plot.plot({
+            width,
             grid: true,
             style: {
                 background: 'transparent',
@@ -53,34 +40,12 @@ export const BeatmapDensityVisualisation = ({ beatmap }) => {
             ]
         });
 
-        chart.setAttribute('font-size', 10);
-
         containerRef.current.append(chart);
 
         return () => chart.remove();
-    }, [ actions ]);
+    }, [ actions, containerRef ]);
 
     return (<>
-        <div ref={containerRef} className={'text-gray-200 text-2xl'} />
+        <div ref={containerRef} className={'text-gray-200'} />
     </>);
 };
-
-// export const BeatmapDensityVisualisation = ({ beatmap }) => {
-//     const density = getActionDensity(eventsToActions(beatmap.events));
-//     const data = density.map((d, i) => ({ time: i, density: d }))
-//         .filter((d) => d);
-
-//     return (<>
-//         <ResponsiveContainer width={'100%'} height={'40%'} minHeight={300}>
-//             <LineChart
-//                 width={500}
-//                 height={300}
-//                 data={data}
-//             >
-//                 <CartesianGrid style={{ stroke: colors.gray[700] }} />
-
-//                 <Line dot={false} type={'basis'} dataKey={'density'} stroke={colors.indigo[500]} />
-//             </LineChart>
-//         </ResponsiveContainer>
-//     </>);
-// };
